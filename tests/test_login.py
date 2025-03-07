597a468d46a1ab6b16ca2c0055120135ad3df24c
= <adityajai243@gmail.com>
2025-03-07 16:14:24 +0530

    pytest.mark.parametrize

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,6 @@
+import pytest
 from assertpy import assert_that
-
+import pytest
 from base.webdriver_wrapper import AutomationWrapper
 from selenium.webdriver.common.by import By
 
@@ -10,3 +11,14 @@ class TestLogin(AutomationWrapper):
         self.driver.find_element(By.CLASS_NAME,"oxd-button").click()
         actual_text = self.driver.find_element(By.XPATH,"//p[contains(normalize-space(),'Quick')]").text
         assert_that("Quick Launch").is_equal_to(actual_text)
+
+    @pytest.mark.parametrize("username,password,expected_error",[
+                                    ["john","john123","Invalid credentials"],
+                                    ["steve","smith123","Invalid credentials"]
+                             ])
+    def test_invalid_login(self,username,password,expected_error):
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "password").send_keys(password)
+        self.driver.find_element(By.CLASS_NAME, "oxd-button").click()
+        actual_text = self.driver.find_element(By.XPATH, "//p[contains(normalize-space(),'Invalid')]").text
+        assert_that("Invalid credentials").is_equal_to(actual_text)
